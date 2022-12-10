da53528d0a0088a1a82aa5432098604b16704dc1
NguyenDinhTra <nguyendinhtra78@gmail.com>
2022-12-10 10:21:53 +0700

    first commit

---
+++ app/src/main/java/com/example/nguyendinhtra_cayxanh/User.java
@@ -0,0 +1,15 @@
+package com.example.nguyendinhtra_cayxanh;
+
+public class User {
+    public String Ten, Email, Tuoi;
+
+    public User(){
+
+    }
+    public User(String Ten, String Email, String Tuoi){
+        this.Email=Email;
+        this.Ten=Ten;
+        this.Tuoi = Tuoi;
+
+    }
+}
